a721f641bb97ab4ebf9259a7f46226690b2dfc83
Lev Valuev <example@example.com>
2023-01-13 05:22:58 -0500

    backend folder updated, eks creation added

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-storage-011223"
+    bucket = "terraform-state-storage-011323"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -20,9 +20,15 @@ module "vpc" {
   source = "./vpc"
 
   cidr                = "172.16.0.0/16"
-  availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  availability_zones  = ["us-east-1a", "us-east-1b"]
   private_subnets     = ["172.16.1.0/24", "172.16.3.0/24"]
   public_subnets      = ["172.16.2.0/24", "172.16.4.0/24"]
   private_subnet_tags = { "kubernetes.io/role/internal-elb" = 1 }
   public_subnet_tags  = { "kubernetes.io/role/elb" = 1 }
+}
+
+module "eks" {
+  source = "./eks"
+
+  cluster_subnets = module.vpc.private_subnets
 }
