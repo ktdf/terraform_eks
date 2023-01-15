291e777d0ad51c89ba8e1c9a51a0e72c3f8b7d31
Lev Valuev <example@example.com>
2023-01-15 07:33:28 -0500

    variables for nod groups, cluster name, addons added. It is possible to specify subnets from the main module

--- eks/variables.tf
+++ eks/variables.tf
@@ -3,3 +3,36 @@ variable "cluster_subnets" {
   type        = list(string)
   default     = []
 }
+
+variable "node_subnets" {
+  description = "List of cluster subnets"
+  type        = list(string)
+  default     = []
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type = string
+  default = "testcluster"
+}
+
+variable "node_group_size" {
+  description = "Size for the node group"
+  type        = map(string)
+  default = {
+    "desired" = 2,
+    "min"     = 1,
+    "max"     = 2
+  }
+
+}
+
+variable "addons" {
+  description = "List of eks addon to be installed"
+  type        = list(string)
+  default = [
+    "coredns",
+    "kube-proxy",
+    "vpc-cni"
+  ]
+}
